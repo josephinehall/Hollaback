c8617bee333556058ae270a6c7735deeb6fe0be7
Amy Palamountain <amy@palamouta.in>
2012-03-03 13:20:43 +1300

    added application view models for user login page and underlying user object with user save and read functions for phonegap

--- iphone/www/main.js
+++ iphone/www/main.js
@@ -1,14 +1,27 @@
 
-
 function onBodyLoad()
 {		
     document.addEventListener("deviceready", onDeviceReady, false);
 }
 
+//document ready wins
+$(document).ready(function(){
+//	var userInformation = new model.userInformation();
+//	userInformation.read();
+//	userInformation.hasUserData();
+//	
+//	
+//	ko.applyBindings(new viewModel.loginViewModel(userInformation));
 
-// PhoneGap is ready
+});
+
+
+// PhoneGap is ready seconds
 function onDeviceReady() {
-    navigator.notification.alert("PhoneGap is working");
-   	alert("device ready");
-	var foo = new yourNamespace.foo();
+   	var userInformation = new model.userInformation();
+	userInformation.read();
+	userInformation.hasUserData();
+    
+	ko.applyBindings(new viewModel.loginViewModel(userInformation));
 }
+
--- iphone/www/viewmodels/applicationviewmodel.js
+++ iphone/www/viewmodels/applicationviewmodel.js
@@ -1,12 +1,83 @@
-var yourNamespace = {
+var model = {
 
-    foo: function() {
-    	alert("foo");
+    userInformation: function() {
+    	
+    	var userName;
+    	var password;
+    	var emailAddress;
+    	var location;
+    	
+        var userNameStorageKey = "userNameKey";
+        var passwordStorageKey = "passwordKey";
+        var emailStorageKey = "emailKey";
+        var locationStorageKey = "locationKey";
+        
+    	this.getUserName = function(){
+    		return userName;
+    	};
+    	
+    	this.setUserName = function(newUserName){
+    		alert(newUserName);
+    		userName = newUserName;
+    	}
+    	
+    	this.read = function(){
+			userName = window.localStorage.getItem(userNameStorageKey);
+			password = window.localStorage.getItem(passwordStorageKey);
+			email = window.localStorage.getItem(emailStorageKey);
+			location = window.localStorage.getItem(locationStorageKey);;
+		};
+		
+		this.save = function(){
+            window.localStorage.setItem(userNameStorageKey, userName);
+            window.localStorage.setItem(passwordStorageKey, password);
+            window.localStorage.setItem(emailStorageKey, emailAddress);
+            window.localStorage.setItem(locationStorageKey, location);
+            
+		};
+		
+		this.hasUserData = function(){
+			alert("Checking if we have user data");
+			if(userName && password && emailAddress && location){
+				return true;
+			}else{
+				return false;
+			}
+		};
+        
+        this.removeCredentials = function(){
+            window.localStorage.removeItem(userNameStorageKey);
+            window.localStorage.removeItem(passwordStorageKey);
+            window.localStorage.removeItem(emailStorageKey);
+            window.localStorage.removeItem(locationStorageKey);
+        };
+        
+        
+		
     },
 
-    bar: function() {
+    userLocation: function() {
+    
+    	this.getAvalibleLocations = function(){};
     }
 };
 
+var viewModel ={
 
+	loginViewModel: function(userModel){
+	
+		self.model = userModel;
+		self.userName = ko.observable(userModel.getUserName());
+		
+		self.submitUserInformation = function() {
+			alert("validate before submit");
+			self.model.setUserName(self.userName());
+			self.model.save();
+        };
+
+		
+	},
+	
+
+};
 
