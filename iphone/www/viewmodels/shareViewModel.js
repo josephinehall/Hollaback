cf4b1ce1035490f1e6860ba8238e18ebbb5c74e7
Josephine Hall <josephinehall@gmail.com>
2012-03-16 19:13:03 +1300

    New graphics, added new bindings to share story view model

--- iphone/www/viewmodels/shareViewModel.js
+++ iphone/www/viewmodels/shareViewModel.js
@@ -2,12 +2,18 @@ var shareViewModels = {
 
 	shareStoryViewModel: function(){
 		var self = this;    
+   
 		 
-        self.bystander = ko.observable("false");
-        self.cool = ko.observable("bitch'n burritos");
+        self.bystander = ko.observable("bystander");
         
         self.harassmentTypes = ko.observableArray(["verbal","stalking","homophobic","transphobic","assault","groping","racist","other"]);
-        self.useGPS = ko.observable(true);
+        
+        self.useGPS = ko.observable("useGPS");
+        
+        self.manualAddress = ko.observable();
+        self.verifyAddress = ko.observable();
+        self.uploadPhoto = ko.obserable();
+        self.story = ko.observable();
 	 },
 }
      
